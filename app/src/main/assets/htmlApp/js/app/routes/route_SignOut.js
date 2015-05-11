a61183303e514760cc2ab2079882f5b8636b7db5
adrian <adrianhyk@gmail.com>
2015-05-11 17:47:48 +0800

    Previous push message was wrong: what we've actually done ton the last commit was to add a feature to save the globalSettings as a JSON string into the app's shared preferences. additionally, we've added a reusableAssets module that has a loaderAnim that will be used on routes by default while loading routes that take a long time (news)

--- app/src/main/assets/htmlApp/js/app/routes/route_SignOut.js
+++ app/src/main/assets/htmlApp/js/app/routes/route_SignOut.js
@@ -1,7 +1,7 @@
 var route = new Router().route;
 
 route("#SignOut", function (event, $thisContainer){
-    //if the loading animation is NOT in thisContainer, that means this route was already loaded previously. no need to reload since the contents wont change
+     //if the loading animation is NOT in thisContainer, that means this route was already loaded previously. no need to reload since the contents wont change
     if($thisContainer.children(".loaderAnim").length != 1){
         console.log("no need to redraw startScraper page since its already been loaded");
         return;     /*no need to redraw this page since its already been loaded*/
